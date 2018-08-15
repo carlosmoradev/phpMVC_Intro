54af443c44e657d545fa419f551acdaf5190f689
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-15 15:50:42 -0500

    starting mvc

---
+++ views/template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template Plantilla</title>
+</head>
+<body>
+    
+    <header>
+    <h1>LOGOTIPO</h1>
+    </header>
+
+<nav>
+    <ul>
+        <li>
+        <a href="">inicio</a>
+        </li>
+        <li>
+        <a href="">Nosotros</a>
+        </li>
+        <li>
+        <a href="">Servicios</a>
+        </li>
+        <li>
+        <a href="">Contactenos</a>
+        </li>
+        </ul
+></nav>
+
+</body>
+</html>
