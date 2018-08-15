21dffcc0adc123ad58bb396a34ba02f5452e9ead
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-15 16:05:15 -0500

    controlador y vista enlazadas al index

--- views/template.php
+++ views/template.php
@@ -14,20 +14,14 @@
 
 <nav>
     <ul>
-        <li>
-        <a href="">inicio</a>
-        </li>
-        <li>
-        <a href="">Nosotros</a>
-        </li>
-        <li>
-        <a href="">Servicios</a>
-        </li>
-        <li>
-        <a href="">Contactenos</a>
-        </li>
-        </ul
-></nav>
+        <li><a href="">inicio</a></li>
+        <li><a href="">Nosotros</a></li>
+        <li><a href="">Servicios</a></li>
+        <li><a href="">Contactenos</a></li>
+    </ul>
+</nav>
+
+<section>PAGINA DE INICIO</section>
 
 </body>
 </html>
