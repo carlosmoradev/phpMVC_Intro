d85f962d0715a42da579adbef1081b4bfee16afa
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-15 17:12:36 -0500

    adding readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Practica patron MVC
+
+Este repositorio contiene una pequeña practica de uso del patron MVC en PHP.
+
+Se manejan las peticiones por GET para simplificar el proceso.
--- views/modules/navegacion.php
+++ views/modules/navegacion.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-        <li><a href="">inicio</a></li>
-        <li><a href="">Nosotros</a></li>
-        <li><a href="">Servicios</a></li>
-        <li><a href="">Contactenos</a></li>
+        <li><a href="index.php">inicio</a></li>
+        <li><a href="index.php?action=nosotros.php">Nosotros</a></li>
+        <li><a href="index.php?action=servicios.php">Servicios</a></li>
+        <li><a href="index.php?action=contactenos.php">Contactenos</a></li>
     </ul>
 </nav>
